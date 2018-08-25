2d2ffdae0b012b4fa29a3969641f67c484fd4994
vilic <i@vilic.info>
2018-08-25 14:39:00 +0800

    Renaming type param

--- library/mutation.d.ts
+++ library/mutation.d.ts
@@ -2,34 +2,38 @@ import {Primitive} from './types';
 
 // Extract keys
 
-export type KeyOfValueWithType<T extends object, TValue> = {
-  [K in keyof T]: T[K] extends TValue ? K : never
-}[keyof T];
+export type KeyOfValueWithType<TObject extends object, TValue> = {
+  [K in keyof TObject]: TObject[K] extends TValue ? K : never
+}[keyof TObject];
 
-export type KeyOfValueNotWithType<T extends object, TValue> = {
-  [K in keyof T]: T[K] extends TValue ? never : K
-}[keyof T];
+export type KeyOfValueNotWithType<TObject extends object, TValue> = {
+  [K in keyof TObject]: TObject[K] extends TValue ? never : K
+}[keyof TObject];
 
 // Keep or omit values by keys
 
 export type KeepValueOfKey<
-  T extends object,
-  TKey extends keyof T
-> = T extends object ? Pick<T, Extract<keyof T, TKey>> : never;
+  TObject extends object,
+  TKey extends keyof TObject
+> = TObject extends object
+  ? Pick<TObject, Extract<keyof TObject, TKey>>
+  : never;
 
 export type OmitValueOfKey<
-  T extends object,
-  TKey extends keyof T
-> = T extends object ? Pick<T, Exclude<keyof T, TKey>> : never;
+  TObject extends object,
+  TKey extends keyof TObject
+> = TObject extends object
+  ? Pick<TObject, Exclude<keyof TObject, TKey>>
+  : never;
 
 // Keep or omit values with given type
 
-export type KeepValueWithType<T extends object, TValue> = {
-  [K in KeyOfValueWithType<T, TValue>]: T[K]
+export type KeepValueWithType<TObject extends object, TValue> = {
+  [K in KeyOfValueWithType<TObject, TValue>]: TObject[K]
 };
 
-export type OmitValueWithType<T extends object, TValue> = {
-  [K in KeyOfValueNotWithType<T, TValue>]: T[K]
+export type OmitValueWithType<TObject extends object, TValue> = {
+  [K in KeyOfValueNotWithType<TObject, TValue>]: TObject[K]
 };
 
 // Miscellaneous
