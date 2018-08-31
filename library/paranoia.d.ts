d2197849ee354e913e60e4428b0c8a3a9526ce93
vilic <i@vilic.info>
2018-08-31 16:46:01 +0800

    Improve EmptyObjectPatch

--- library/paranoia.d.ts
+++ library/paranoia.d.ts
@@ -1,11 +1,11 @@
 // tslint:disable:class-name variable-name
 
 declare class __Type<T> {
-  private __type: T;
+  protected _: T;
 }
 
 export type Nominal<T, TName> = T & __Type<TName>;
 
 export class EmptyObjectPatch {
-  protected _?: never;
+  protected _?: unknown;
 }
