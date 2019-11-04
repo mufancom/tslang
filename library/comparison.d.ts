ec67875ea5b2aea9785145cbc48651132b3016e6
vilic <i@vilic.info>
2019-11-04 18:49:19 +0800

    Simplify IsEqual

--- library/comparison.d.ts
+++ library/comparison.d.ts
@@ -1,5 +1,3 @@
-export type IsEqual<TA, TB> = [TA, keyof TA] extends [TB, keyof TB]
-  ? ([TB, keyof TB] extends [TA, keyof TA] ? true : false)
-  : false;
+export type IsEqual<TA, TB> = [TA, TB] extends [TB, TA] ? true : false;
 
 export type IsCompatible<T, TComparison> = T extends TComparison ? true : false;
