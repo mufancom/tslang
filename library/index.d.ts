34f492a6e730103a8011728c28b43b592a0e9df4
vilic <i@vilic.info>
2020-01-29 02:18:32 +0800

    Add mixin utility function

--- library/index.d.ts
+++ library/index.d.ts
@@ -1,5 +1,19 @@
+import {Constructor} from './types';
+
 export * from './assertion';
 export * from './comparison';
 export * from './types';
 export * from './paranoia';
 export * from './mutation';
+
+/**
+ * https://www.typescriptlang.org/docs/handbook/mixins.html
+ * @param targetConstructor Target constructor of which the prototype will be
+ * applied with properties from other constructors.
+ * @param constructors Constructors of which the properties will be applied to
+ * the target constructor.
+ */
+export function mixin(
+  targetConstructor: Constructor,
+  ...constructors: Constructor[]
+): void;
--- library/index.js
+++ library/index.js
@@ -1,1 +1,24 @@
-// fake library entry
+/* eslint-disable @magicspace/scoped-modules */
+
+// @ts-check
+
+/**
+ * @typedef {import('./types').Constructor} Constructor
+ */
+
+/**
+ * @param targetConstructor {Constructor}
+ * @param constructors {Constructor[]}
+ */
+exports.mixin = function mixin(targetConstructor, ...constructors) {
+  let targetPrototype = targetConstructor.prototype;
+
+  for (let constructor of constructors) {
+    let prototype = constructor.prototype;
+
+    Object.defineProperties(
+      targetPrototype,
+      Object.getOwnPropertyDescriptors(prototype),
+    );
+  }
+};
