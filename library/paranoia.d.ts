0982bee83b990bd3be800284621c897519856fe9
vilicvane <vilicvane@live.com>
2021-10-14 08:29:55 +0800

    Fix Denominalize

--- library/paranoia.d.ts
+++ library/paranoia.d.ts
@@ -1,19 +1,19 @@
-import {Primitive} from './types';
-
-declare const __type: unique symbol;
+declare const __nominal_name: unique symbol;
+declare const __nominal_type: unique symbol;
 
 // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
-type __Type<T> = {
-  [__type]: T;
+type __Type<TName, T> = {
+  [__nominal_name]: TName;
+  [__nominal_type]: T;
 };
 
-export type Nominal<T, TName> = T & __Type<TName>;
+export type Nominal<T, TName> = T & __Type<TName, T>;
 
-export type Denominalize<T> = T extends Nominal<infer TDenominalized, any>
+export type Denominalize<T> = T extends __Type<any, infer TDenominalized>
   ? TDenominalized
   : T;
 
-export type DenominalizeDeep<T> = T extends Nominal<infer TDenominalized, any>
+export type DenominalizeDeep<T> = T extends __Type<any, infer TDenominalized>
   ? TDenominalized
   : {[TKey in keyof T]: DenominalizeDeep<T[TKey]>};
 
